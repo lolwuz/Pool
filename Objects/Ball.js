65a6af857dee4dd7020dceee8ef0be02e0b0c217
Koen <koenzk@hotmail.com>
2017-10-06 10:24:13 +0200

    Merge fix

--- Objects/Ball.js
+++ Objects/Ball.js
@@ -5,8 +5,13 @@
 class Ball extends THREE.Mesh {
     constructor(ballNumber, position) {
         const sphereGeometry = new THREE.SphereGeometry(0.5, 128, 128);
-        const texture = new THREE.TextureLoader().load( "./textures/ball/" + ballNumber + ".png" );
-        const material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, map: texture, shininess: 10, specular: 0xFFFFFF });
+        const texture = new THREE.TextureLoader().load("./textures/ball/" + ballNumber + ".png");
+        const material = new THREE.MeshPhongMaterial({
+            color: 0xFFFFFF,
+            map: texture,
+            shininess: 10,
+            specular: 0xFFFFFF
+        });
         super(sphereGeometry, material);
         this.castShadow = true;
         this.speed = new THREE.Vector2();
@@ -15,8 +20,22 @@ class Ball extends THREE.Mesh {
         this.pocketed = false; // boolean for if the ball is pocketed (needed? hmm, not sure)
     };
 
+    static isNumNearOtherNum(num1, num2, pixels) {
+
+        for (let i = 0.00; i <= 0.50; i += 0.01) {
+
+            console.log(num1 + " " + num2 + " " + i);
+
+            if (num1.toFixed(2) === (num2 - i).toFixed(2) || num1.toFixed(2) === (num2 + i).toFixed(2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     setSpeed(speed) {
-        this.speed = speed; 
+        this.speed = speed;
     };
 
     setPosition(position) {
@@ -29,9 +48,9 @@ class Ball extends THREE.Mesh {
         let dy = Ball.position.y - this.position.y;
         let distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 
-        if(distance < 1) { // Collision!
+        if (distance < 1) { // Collision!
             // Play sound
-            //sound.play();    
+            //sound.play();
             let movementAngleA = Math.atan2(this.speed.y, this.speed.x);
             let movementAngleB = Math.atan2(Ball.speed.y, Ball.speed.x);
             let velocityA = Math.sqrt(Math.pow(this.speed.y, 2) + Math.pow(this.speed.x, 2));
@@ -39,14 +58,14 @@ class Ball extends THREE.Mesh {
             let collisionAngle = Math.atan2(dy, dx);
 
             let aSpeedX = (2 * velocityB * Math.cos(movementAngleB - collisionAngle) / 2)
-                * Math.cos(collisionAngle) + velocityA * Math.sin(movementAngleA - collisionAngle) * Math.cos(collisionAngle + Math.PI / 2 );
+                * Math.cos(collisionAngle) + velocityA * Math.sin(movementAngleA - collisionAngle) * Math.cos(collisionAngle + Math.PI / 2);
             let aSpeedY = (2 * velocityB * Math.cos(movementAngleB - collisionAngle) / 2)
-                * Math.sin(collisionAngle) + velocityA * Math.sin(movementAngleA - collisionAngle) * Math.sin(collisionAngle + Math.PI / 2 );    
-                
+                * Math.sin(collisionAngle) + velocityA * Math.sin(movementAngleA - collisionAngle) * Math.sin(collisionAngle + Math.PI / 2);
+
             let bSpeedX = (2 * velocityA * Math.cos(movementAngleA - collisionAngle) / 2)
-                * Math.cos(collisionAngle) + velocityB * Math.sin(movementAngleB - collisionAngle) * Math.cos(collisionAngle + Math.PI / 2 );
+                * Math.cos(collisionAngle) + velocityB * Math.sin(movementAngleB - collisionAngle) * Math.cos(collisionAngle + Math.PI / 2);
             let bSpeedY = (2 * velocityA * Math.cos(movementAngleA - collisionAngle) / 2)
-                * Math.sin(collisionAngle) + velocityB * Math.sin(movementAngleB - collisionAngle) * Math.sin(collisionAngle + Math.PI / 2 );
+                * Math.sin(collisionAngle) + velocityB * Math.sin(movementAngleB - collisionAngle) * Math.sin(collisionAngle + Math.PI / 2);
 
             // Reset position outside of collision bounds.
             this.position.x = Ball.position.x - (Math.round(Math.cos(collisionAngle) * 1000) / 1000);
@@ -60,16 +79,16 @@ class Ball extends THREE.Mesh {
     checkCollisionTable(Table) {
         let direction = new THREE.Vector3(this.speed.x, this.speed.y, 0);
         this.raycaster.set(this.position, direction);
-        
+
         let intersections = this.raycaster.intersectObjects(Table.children);
-        
-        for(let i = 0; i < intersections.length; i++) {
+
+        for (let i = 0; i < intersections.length; i++) {
             let intersection = intersections[i];
             let dx = intersection.point.x - this.position.x;
             let dy = intersection.point.y - this.position.y;
             let distanceOfIntersect = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 
-            if(distanceOfIntersect < 0.5) {
+            if (distanceOfIntersect < 0.5) {
                 console.log("hit");
 
                 if (Ball.isNumNearOtherNum(this.position.x, Table.dimensions.topLeft.x)) {
@@ -95,69 +114,44 @@ class Ball extends THREE.Mesh {
                 let newDirection = direction.reflect(intersection.face.normal);
                 // Set new direction
                 newDirection.reflect(intersection.face.normal);
-                let newDirection = new THREE.Vector3(direction.x,direction.y,direction.z); 
-                
+                let newDirection = new THREE.Vector3(direction.x, direction.y, direction.z);
+
                 this.speed.x = newDirection.x;
                 this.speed.y = newDirection.y;
             }
-            
-                let antiNormal = new THREE.Vector3(intersection.face.normal.x, intersection.face.normal.y, intersection.face.normal.z);
-                
-                // Invert direction 
-                this.raycaster.set(this.position, antiNormal); // setting a new raycaster
-                let lastIntersect = this.raycaster.intersectObjects(Table.children);
-     
-                let newPos = lastIntersect[0].point.add(antiNormal.multiplyScalar(-0.5));
-                console.log(newPos);
-                this.position.x = newPos.x;
-                this.position.y = newPos.y;
-            } 
-        }
 
-        // Reverse speed when boundary off the table have been reached.
-        /*
-        if( this.position.x + 1 > Table.dimensions.topRight.x || this.position.x - 1 < Table.dimensions.topLeft.x){
-            this.speed.x *= -1;
+            let antiNormal = new THREE.Vector3(intersection.face.normal.x, intersection.face.normal.y, intersection.face.normal.z);
+
+            // Invert direction
+            this.raycaster.set(this.position, antiNormal); // setting a new raycaster
+            let lastIntersect = this.raycaster.intersectObjects(Table.children);
+
+            let newPos = lastIntersect[0].point.add(antiNormal.multiplyScalar(-0.5));
+            console.log(newPos);
+            this.position.x = newPos.x;
+            this.position.y = newPos.y;
         }
-        if( this.position.y + 1 > Table.dimensions.topRight.y || this.position.y - 1 < Table.dimensions.bottomRight.y){      
-            this.speed.y *= -1;
-        }  
-        */
-    };
+    }
 
     move(deltaTime) {
         // Setting rolling resistance.
         this.speed.x = this.speed.x * (1 - 0.01 * deltaTime);
         this.speed.y = this.speed.y * (1 - 0.01 * deltaTime);
-     
+
         // Set new position according to x/y speed.
         let stepX = this.speed.x * deltaTime;
         let stepY = this.speed.y * deltaTime;
-        this.position.set( this.position.x + stepX, this.position.y + stepY, 0 );
+        this.position.set(this.position.x + stepX, this.position.y + stepY, 0);
 
         // Update ball rotation
         let tempMat = new THREE.Matrix4();
-        tempMat.makeRotationAxis(new THREE.Vector3(0,1,0), stepX/ 0.5);
+        tempMat.makeRotationAxis(new THREE.Vector3(0, 1, 0), stepX / 0.5);
         tempMat.multiply(this.matrix);
         this.matrix = tempMat;
         tempMat = new THREE.Matrix4();
-        tempMat.makeRotationAxis(new THREE.Vector3(1,0,0), -stepY/0.5);
+        tempMat.makeRotationAxis(new THREE.Vector3(1, 0, 0), -stepY / 0.5);
         tempMat.multiply(this.matrix);
         this.matrix = tempMat;
         this.rotation.setFromRotationMatrix(this.matrix);
-    };
-
-    static isNumNearOtherNum(num1, num2, pixels) {
-
-        for (let i = 0.00; i <= 0.50; i+= 0.01) {
-
-            console.log(num1 + " " + num2 + " " + i);
-
-            if (num1.toFixed(2) === (num2 - i).toFixed(2) || num1.toFixed(2) === (num2 + i).toFixed(2)) {
-                return true;
-            }
-        }
-
-        return false;
     }
-} 
+}
